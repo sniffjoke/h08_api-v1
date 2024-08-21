dd501cc686489e1571cb8d0bd1017e43c75b1206
sniffjoke <sniffjoke@gmail.com>
2024-08-21 23:03:53 +0200

    add new methods and auth-service for auth

--- src/middlewares/errorApiMiddleware.ts
+++ src/middlewares/errorApiMiddleware.ts
@@ -4,7 +4,7 @@ import {ApiError} from "../exceptions/api.error";
 
 export const errorApiMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof ApiError) {
-        return res.status(err.status).json({ErrorsMessage: [{message: err.message, field: err.field}]})
+        return res.status(err.status).json({errorsMessages: [{message: err.message, field: err.field}]})
     }
     return res.status(500).json({message: 'Непредвиденная ошибка', field: 'unknown field'})
 }
