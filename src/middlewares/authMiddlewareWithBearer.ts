119987d0e47b9076e5f22786d1aab5cd55389166
sniffjoke <sniffjoke@gmail.com>
2024-08-23 23:12:07 +0200

    edit for tests

--- src/middlewares/authMiddlewareWithBearer.ts
+++ src/middlewares/authMiddlewareWithBearer.ts
@@ -6,19 +6,23 @@ import {SETTINGS} from "../settings";
 export const authMiddlewareWithBearer = (req: Request, res: Response, next: NextFunction) => {
     let token = req.headers.authorization
     if (!token) {
-        return next(ApiError.UnauthorizedError())
+        // return next(ApiError.UnauthorizedError())
+        return next(ApiError.AnyUnauthorizedError('1'))
     }
     try {
         token = token!.split(' ')[1]
         if (token === null || !token) {
-            return next(ApiError.UnauthorizedError())
+            // return next(ApiError.UnauthorizedError())
+            return next(ApiError.AnyUnauthorizedError('2'))
         }
         let verifyToken = jwt.verify(token, SETTINGS.VARIABLES.JWT_SECRET_ACCESS_TOKEN as string)
         if (!verifyToken) {
-            return next(ApiError.UnauthorizedError())
+            // return next(ApiError.UnauthorizedError())
+            return next(ApiError.AnyUnauthorizedError('3'))
         }
         next()
     } catch (e) {
-        return next(ApiError.UnauthorizedError())
+        // return next(ApiError.UnauthorizedError())
+        return next(ApiError.AnyUnauthorizedError('4'))
     }
 }
