fd8b0a3f013e6873a6291d36b201c6863e6f34fe
sniffjoke <sniffjoke@gmail.com>
2024-08-24 22:20:58 +0200

    edit for tests

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -133,7 +133,8 @@ export const refreshTokenController = async (req: Request, res: Response, next: 
 export const removeRefreshTokenController = async (req: Request, res: Response, next: NextFunction) => {
     try {
         // const token = req.headers.cookie?.split('=')[1] as string
-        const token = Object.values(req.cookies)[0]
+        // const token = Object.values(req.cookies)[0]
+        const token = req.cookies.refreshToken as string
         const tokenFromDb = await tokenCollection.findOne({refreshToken: token as string})
         if (!tokenFromDb || tokenFromDb.blackList) {
             next(ApiError.AnyUnauthorizedError(token))
