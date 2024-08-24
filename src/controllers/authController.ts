94fb6bdbd653bbe77980d8cebcc88d62a40a0869
sniffjoke <sniffjoke@gmail.com>
2024-08-24 16:20:17 +0200

    edit for tests

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -136,7 +136,7 @@ export const removeRefreshTokenController = async (req: Request, res: Response, 
         // const token = req.cookies.refreshToken.split(';')[0] as string
         const tokenFromDb = await tokenCollection.findOne({refreshToken: token})
         if (!tokenFromDb) {
-            next(ApiError.AnyUnauthorizedError(req.headers.cookie))
+            next(ApiError.AnyUnauthorizedError(req.headers.cookie?.split(';')[0]))
         } else {
             await tokenCollection.updateOne({refreshToken: tokenFromDb?.refreshToken}, {$set: {blackList: true}})
             res.clearCookie('refreshToken')
